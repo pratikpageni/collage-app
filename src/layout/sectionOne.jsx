b04b25cee83e46d254920189ae23592ef87fc74a
pratikpageni <pagenipratik@gmail.com>
2022-01-12 18:26:47 +0545

    fix(slide):slide updated new component added slide

---
+++ src/layout/sectionOne.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const sectionOne = () => {
+    return (
+        <>
+            <div className='flex mb-4'>
+                <div className='w-1/4'>
+                Notice and Apply
+                </div>
+                <div className='w-3/4>'>
+
+                </div>
+            </div>
+
+
+        </>
+    );
+};
+
+export default sectionOne;
